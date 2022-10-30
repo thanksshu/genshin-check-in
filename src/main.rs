5216a69b34730369a9b522a2e112eac1f224fab9
thanksshu <thanksshu@github.com>
2022-10-30 12:09:22 +0100

    change to ureq, change to std server

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,43 @@
-use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use cookie_store::CookieStore;
 use log::{error, info};
-use reqwest::{cookie::Jar, ClientBuilder, Url};
 use serde::Deserialize;
-use std::{env, sync::Arc};
+use std::{
+    env::var,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+use ureq::{builder, Cookie};
+use url::Url;
 
 const URL: &str = "https://hk4e-api-os.mihoyo.com/event/sol/sign?act_id=e202102251931481";
 const UA: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/100.0";
 
-#[derive(Deserialize, Debug)]
-struct CheckInResponse {
-    message: String,
-    retcode: i32,
-}
+fn check_in() -> Result<String, Box<dyn std::error::Error>> {
+    #[derive(Deserialize, Debug)]
+    struct CheckInResponse {
+        message: String,
+        retcode: i32,
+    }
 
-async fn check_in() -> Result<String, Box<dyn std::error::Error>> {
     let url = URL.parse::<Url>()?; // parse check in url
 
     /* bake cookies */
-    let ltuid = env::var("LTUID")?;
-    let ltoken = env::var("LTOKEN")?;
+    let mut jar = CookieStore::default();
 
     let cookies = [
-        format!("ltoken={}; Domain=.mihoyo.com;", ltoken),
-        format!("ltuid={}; Domain=.mihoyo.com;", ltuid),
+        Cookie::new("ltoken", var("LTOKEN")?),
+        Cookie::new("ltuid", var("LTUID")?),
     ];
 
-    /* add cookies to jar */
-    let jar = Arc::new(Jar::default());
-    cookies
-        .iter()
-        .for_each(|cookie| jar.add_cookie_str(cookie, &url));
+    cookies.iter().for_each(|cookie| {
+        jar.insert_raw(cookie, &url).unwrap();
+    });
 
     /* build client */
-    let client = ClientBuilder::new()
-        .user_agent(UA)
-        .cookie_provider(jar.clone())
-        .build()?;
+    let client = builder().user_agent(UA).cookie_store(jar).build();
 
     /* post request */
-    let result = client
-        .post(url)
-        .send()
-        .await?
-        .json::<CheckInResponse>()
-        .await?;
+    let result = client.post(URL).call()?.into_json()?;
 
     /* verify response */
     match result {
@@ -55,36 +49,44 @@ async fn check_in() -> Result<String, Box<dyn std::error::Error>> {
     }
 }
 
-/*  app for POST /invoke */
-#[post("/invoke")]
-async fn invoke() -> impl Responder {
-    match check_in().await {
-        Ok(m) => {
-            let message = m.to_string();
-            info!("{}", message);
-            HttpResponse::Ok()
-                .insert_header(("x-fc-status", "200"))
-                .body(m)
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buf = [0_u8; 2048];
+    let n = stream.read(&mut buf[..]).unwrap();
+    let receive = String::from_utf8_lossy(&buf[..n]);
+    if receive.starts_with("POST /invoke") {
+        match check_in() {
+            Ok(message) => {
+                info!("{}", message);
+                stream.write_all(
+                    format!("HTTP/1.1 200 OK\r\nX-Fc-Status: 200\r\n\r\n{}", message).as_bytes(),
+                )?;
+            }
+            Err(message) => {
+                error!("{}", message);
+                stream.write_all(
+                    format!(
+                        "HTTP/1.1 404 Not Found\r\nX-Fc-Status: 404\r\n\r\n{}",
+                        message
+                    )
+                    .as_bytes(),
+                )?;
+            }
         }
-
-        Err(e) => {
-            let message = format!("{}", e);
-            error!("{}", message);
-            HttpResponse::NotFound()
-                .insert_header(("x-fc-status", "404"))
-                .body(message)
-        }
+        stream.flush()?;
     }
+    Ok(())
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     /* init logger */
     simple_logger::SimpleLogger::new().env().init().unwrap();
 
     /* start server */
-    HttpServer::new(|| App::new().service(invoke))
-        .bind(("0.0.0.0", 9000))?
-        .run()
-        .await
+    let listener = TcpListener::bind("0.0.0.0:9000").unwrap();
+
+    /* accept connections and process them serially */
+    for stream in listener.incoming() {
+        handle_client(stream?)?;
+    }
+    Ok(())
 }
