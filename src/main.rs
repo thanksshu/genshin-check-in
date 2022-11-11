3048421f754852b2219faa6cdabc345e47d86ff0
thanksshu <thanksshu@github.com>
2022-11-11 17:13:54 +0100

    Add retry support

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,144 @@
-use cookie_store::CookieStore;
-use log::{error, info};
+use anyhow::{Context, Result};
+use once_cell::sync::OnceCell;
+use reqwest::{blocking::Client, cookie::Jar, Url};
 use serde::Deserialize;
 use std::{
     env::var,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    sync::Arc,
+    thread::sleep,
+    time::Duration,
 };
-use ureq::{builder, Cookie};
-use url::Url;
+use tracing::{error, info, warn};
 
-const URL: &str = "https://hk4e-api-os.mihoyo.com/event/sol/sign?act_id=e202102251931481";
-const UA: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/100.0";
+const URL_STRING: &str = "https://hk4e-api-os.mihoyo.com/event/sol/sign?act_id=e202102251931481";
+const UA: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0";
+const RETRY_TIME: u64 = 5;
+const RETRY_INTERVAL: u64 = 1000; // ms
 
-fn check_in() -> Result<String, Box<dyn std::error::Error>> {
-    #[derive(Deserialize, Debug)]
-    struct CheckInResponse {
-        message: String,
-        retcode: i32,
-    }
+static URL: OnceCell<Url> = OnceCell::new();
+static LTUID: OnceCell<String> = OnceCell::new();
+static LTOKEN: OnceCell<String> = OnceCell::new();
 
-    let url = URL.parse::<Url>()?; // parse check in url
+#[derive(Deserialize, Debug)]
+struct CheckInResponse {
+    message: String,
+    retcode: i32,
+}
 
+fn check_in() -> Result<CheckInResponse, reqwest::Error> {
     /* bake cookies */
-    let mut jar = CookieStore::default();
-
     let cookies = [
-        Cookie::new("ltoken", var("LTOKEN")?),
-        Cookie::new("ltuid", var("LTUID")?),
+        format!("ltoken={}; Domain=.mihoyo.com;", LTOKEN.get().unwrap()),
+        format!("ltuid={}; Domain=.mihoyo.com;", LTUID.get().unwrap()),
     ];
 
-    cookies.iter().for_each(|cookie| {
-        jar.insert_raw(cookie, &url).unwrap();
-    });
+    /* add cookies to jar */
+    let jar = Arc::new(Jar::default());
+    cookies
+        .iter()
+        .for_each(|cookie| jar.add_cookie_str(cookie, URL.get().unwrap()));
 
     /* build client */
-    let client = builder().user_agent(UA).cookie_store(jar).build();
+    let client = Client::builder()
+        .user_agent(UA)
+        .cookie_provider(jar)
+        .build()
+        .unwrap();
 
     /* post request */
-    let result = client.post(URL).call()?.into_json()?;
-
-    /* verify response */
-    match result {
-        CheckInResponse {
-            message,
-            retcode: i,
-        } if i == 0 || i == -5003 => Ok(message),
-        CheckInResponse { message, retcode } => Err(format!("{} {}", retcode, message).into()),
-    }
+    Ok(client
+        .post(URL_STRING)
+        .send()?
+        .json::<CheckInResponse>()
+        .unwrap())
 }
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut buf = [0_u8; 2048];
-    let n = stream.read(&mut buf[..]).unwrap();
-    let receive = String::from_utf8_lossy(&buf[..n]);
-    if receive.starts_with("POST /invoke") {
+fn handle_invoke(stream: &mut TcpStream) {
+    let mut rely_404 = |error_message: &String| {
+        stream
+            .write_all(
+                format!(
+                    "HTTP/1.1 404 Not Found\r\nX-Fc-Status: 404\r\n\r\n{}",
+                    error_message
+                )
+                .as_bytes(),
+            )
+            .unwrap();
+    };
+
+    for try_time in 1..=RETRY_TIME {
+        let wait_time = Duration::from_millis(try_time * RETRY_INTERVAL);
         match check_in() {
-            Ok(message) => {
-                info!("{}", message);
-                stream.write_all(
-                    format!("HTTP/1.1 200 OK\r\nX-Fc-Status: 200\r\n\r\n{}", message).as_bytes(),
-                )?;
+            Ok(CheckInResponse { message, retcode }) => {
+                if retcode == 0 || retcode == -5003 {
+                    stream
+                        .write_all(
+                            format!(
+                                "HTTP/1.1 200 OK\r\nX-Fc-Status: 200\r\n\r\n{retcode} {message}"
+                            )
+                            .as_bytes(),
+                        )
+                        .unwrap();
+                    info!("check in succeed or already checked in");
+                    break;
+                } else {
+                    warn!("check in failed, retrying in {:#?}", wait_time);
+                    sleep(wait_time);
+                    if try_time == 5 {
+                        rely_404(&format!("{retcode} {message}"));
+                        error!("all check in retry failed");
+                    }
+                    continue;
+                }
             }
-            Err(message) => {
-                error!("{}", message);
-                stream.write_all(
-                    format!(
-                        "HTTP/1.1 404 Not Found\r\nX-Fc-Status: 404\r\n\r\n{}",
-                        message
-                    )
-                    .as_bytes(),
-                )?;
+            Err(error) => {
+                warn!("check in failed, retrying in {:#?}", wait_time);
+                sleep(wait_time);
+                if try_time == 5 {
+                    rely_404(&format!("{error}"));
+                    error!("all check in retry failed");
+                }
+                continue;
             }
         }
-        stream.flush()?;
     }
-    Ok(())
+    stream.flush().unwrap();
 }
 
-fn main() -> std::io::Result<()> {
+fn main() {
+    /* init statics */
+    LTUID
+        .set(
+            var("LTUID")
+                .context("environment variable \"LTUID\" not present")
+                .unwrap(),
+        )
+        .unwrap();
+    LTOKEN
+        .set(
+            var("LTOKEN")
+                .context("environment variable \"LTOKEN\" not present")
+                .unwrap(),
+        )
+        .unwrap();
+    URL.set(URL_STRING.parse::<Url>().unwrap()).unwrap();
+
     /* init logger */
-    simple_logger::SimpleLogger::new().env().init().unwrap();
+    tracing_subscriber::fmt().init();
 
     /* start server */
     let listener = TcpListener::bind("0.0.0.0:9000").unwrap();
 
     /* accept connections and process them serially */
-    for stream in listener.incoming() {
-        handle_client(stream?)?;
+    for income in listener.incoming() {
+        let mut stream = income.unwrap();
+        let mut buf = [0_u8; 2048];
+        let n = stream.read(&mut buf[..]).unwrap();
+        let receive = String::from_utf8_lossy(&buf[..n]);
+        if receive.starts_with("POST /invoke") {
+            handle_invoke(&mut stream);
+        }
     }
-    Ok(())
 }
